2373dd6916e39338bccf54656a2cb4dc30ef2776
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-01-23 19:40:48 +0600

    2nd commit

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,8 +1,14 @@
 
 const router = require('express').Router();
-const { createUser, getUsers } = require('./user.controller');
+const { createUser, getUsers, getUserById, updateUserInfo, DeleteUserInfo } = require('./user.controller');
 
 
 router.post("/", createUser);
 router.get("/", getUsers);
+router.post("/getUserById", getUserById);
+router.post("/updateUserInfo", updateUserInfo);
+router.post("/deleteUser", DeleteUserInfo);
+
+
+
 module.exports = router;
