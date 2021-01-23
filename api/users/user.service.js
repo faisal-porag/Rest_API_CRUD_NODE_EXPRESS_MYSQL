150e8f0ce85f6d725f97d43ceb1505d1b88f8cd9
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-01-23 19:41:37 +0600

    third commit

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,36 +1,96 @@
 const pool = require('../../db_config/database');
 
 module.exports = {
-    create: (data, callback) =>{
-        pool.query(
-            `insert into personal_info (first_name, last_name, email, gender, password, phone_number) 
-                values (?,?,?,?,?,?)`,
-            [
-                data.first_name,
-                data.last_name,
-                data.email,
-                data.gender,
-                data.password,
-                data.phone_number
-            ],
-            (error, results, fields) =>{
-                if(error){
-                    return callback(error);
-                }
-                return callback(null, results);
-            }
-        );
-    },
-    getUsers: callBack => {
-        pool.query(
-          `select * from personal_info`,
-          [],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
+  create: (data, callBack) =>{
+    pool.query(
+        `insert into personal_info (first_name, last_name, email, gender, password, phone_number) 
+            values (?,?,?,?,?,?)`,
+        [
+            data.first_name,
+            data.last_name,
+            data.email,
+            data.gender,
+            data.password,
+            data.phone_number
+        ],
+        (error, results, fields) =>{
+            if(error){
+                return callBack(error);
             }
             return callBack(null, results);
-          }
-        );
-      },
+        } 
+    );
+  },
+
+  //Get All Users
+  getUsers: callBack => {
+    pool.query(
+      `select first_name, last_name, gender, email, phone_number from personal_info`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+
+  //Get User by ID
+  getUserByUserId: (data, callBack) => {
+    console.log(data.Id)
+    pool.query(
+      `select first_name, last_name, gender, email, phone_number from personal_info where 
+      Id = ?`,
+      [
+        data.Id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+
+  //Update User info
+  updateUserById: (data, callBack)=>{
+    pool.query(
+      `update personal_info set first_name = ?,
+        last_name = ?,
+        email = ?,
+        gender = ?,
+        phone_number = ?
+        where Id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.email,
+        data.gender,
+        data.phone_number,
+        data.Id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    )
+  },
+
+  //Delete User info
+  deleteUserById: (data, callBack)=>{
+    pool.query(
+      `delete from personal_info where Id = ?`,
+      [data.Id],
+      (error, results, fields)=>{
+        if(error){
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    )
+  }
 };
