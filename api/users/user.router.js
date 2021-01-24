a497102acff9d6b77e44c49bda6018dc3042e09e
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-01-24 23:19:30 +0600

    3rd commit

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,14 +1,33 @@
 
 const router = require('express').Router();
-const { createUser, getUsers, getUserById, updateUserInfo, DeleteUserInfo } = require('./user.controller');
+const { 
+    createUser, 
+    getUsers, 
+    getUserById, 
+    updateUserInfo, 
+    DeleteUserInfo, 
+    checkAuthUserByEmail 
+} = require('./user.controller');
 
+const { checkValidToken } = require('../../auth/token_validation');
 
-router.post("/", createUser);
-router.get("/", getUsers);
+//Access without login
+//---------------------------
+//router.post("/addUser", checkValidToken, createUser);
+//router.get("/getAllUser", getUsers);
+//router.post("/getUserById", getUserById);
+//router.post("/updateUserInfo", checkValidToken, updateUserInfo);
+//router.post("/deleteUser", checkValidToken, DeleteUserInfo);
+
+//Route With Authentication (Access with login)
+//-------------------------------------------------
+router.post("/addUser", createUser);
+router.get("/getAllUser", getUsers);
 router.post("/getUserById", getUserById);
 router.post("/updateUserInfo", updateUserInfo);
 router.post("/deleteUser", DeleteUserInfo);
 
-
+//Login Route
+router.post("/login", checkAuthUserByEmail);
 
 module.exports = router;
