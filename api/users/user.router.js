42a687e0be355d9bc2c4b853c13bb5fd444d137a
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-01-29 13:57:34 +0600

    4th commit

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -5,27 +5,29 @@ const {
     getUsers, 
     getUserById, 
     updateUserInfo, 
-    DeleteUserInfo, 
+    DeleteUserInfo,
+    userDetailsInfoById, 
     checkAuthUserByEmail 
 } = require('./user.controller');
 
 const { checkValidToken } = require('../../auth/token_validation');
 
-//Access without login
-//---------------------------
-//router.post("/addUser", checkValidToken, createUser);
-//router.get("/getAllUser", getUsers);
-//router.post("/getUserById", getUserById);
-//router.post("/updateUserInfo", checkValidToken, updateUserInfo);
-//router.post("/deleteUser", checkValidToken, DeleteUserInfo);
-
-//Route With Authentication (Access with login)
-//-------------------------------------------------
-router.post("/addUser", createUser);
+//Access without login 
+//----------------------
+router.post("/addUser", checkValidToken, createUser);
 router.get("/getAllUser", getUsers);
 router.post("/getUserById", getUserById);
-router.post("/updateUserInfo", updateUserInfo);
-router.post("/deleteUser", DeleteUserInfo);
+router.post("/updateUserInfo", checkValidToken, updateUserInfo);
+router.post("/deleteUser", checkValidToken, DeleteUserInfo);
+router.post("/userDetails", userDetailsInfoById);
+
+//Route With Authentication (Access with login)
+//---------------------------------------------
+// router.post("/addUser", createUser);
+// router.get("/getAllUser", getUsers);
+// router.post("/getUserById", getUserById);
+// router.post("/updateUserInfo", updateUserInfo);
+// router.post("/deleteUser", DeleteUserInfo);
 
 //Login Route
 router.post("/login", checkAuthUserByEmail);
